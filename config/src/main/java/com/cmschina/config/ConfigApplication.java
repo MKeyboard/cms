e6dd0d5a5e2d7c4f69b2bc2335510cfb29102614
unknown <1349926002@qq.com>
2020-11-20 15:38:07 +0800

    change

--- config/src/main/java/com/cmschina/config/ConfigApplication.java
+++ config/src/main/java/com/cmschina/config/ConfigApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException {
+        System.setProperty("LOCAL_IP", InetAddress.getLocalHost().getHostAddress());
         SpringApplication.run(ConfigApplication.class, args);
     }
 
